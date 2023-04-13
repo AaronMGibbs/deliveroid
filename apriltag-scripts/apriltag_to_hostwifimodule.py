a49a734686febc2db6951c79d4cd7db73a23504a
Noah Drakes <noahdrakes@Noahs-MacBook-Air.local>
2023-04-13 12:35:29 -0400

    user can input coordinates, robot return to origin method added

--- apriltag-scripts/apriltag_to_hostwifimodule.py
+++ apriltag-scripts/apriltag_to_hostwifimodule.py
@@ -1,5 +1,6 @@
 import serial
 import time
+
 arduino = serial.Serial(port='/dev/cu.usbserial-0001', baudrate=115200, timeout=.1)
 def write_read(x):
     arduino.write(bytes(x, 'utf-8'))
--- apriltag-scripts/apriltag_video.py
+++ apriltag-scripts/apriltag_video.py
@@ -8,9 +8,13 @@ import numpy
 import math
 
 # GLOBALS
-robotPositionedCorrectlySTATE = False
+
+
 
 ################################################################################
+
+def returnRobotState():
+    return robotPositionedCorrectlySTATE
 def apriltag_video(input_streams=['../media/input/single_tag.mp4', '../media/input/multiple_tags.mp4'], # For default cam use -> [0]
                    output_stream=False,
                    display_stream=True,
@@ -36,12 +40,25 @@ def apriltag_video(input_streams=['../media/input/single_tag.mp4', '../media/inp
     location, or specify your own search paths as needed.
     '''
 
+    robotPositionedCorrectlySTATE   = 1
+    ROBOTNOTFACINGDESTINATION       = 1
+    ROBOTFACINGDESTINATION          = 2
+
+    robotCommandInProgress          = 3
+    WAITINGFORCOMMAND               = 3
+    COMMANDINPROGRESS               = 4
+
+    robotReturnToOrigin             = 5
+    ROBOTNOTRETURNING               = 5
+    ROBOTRETURNINGTOORIGIN          = 6  
+
+
 
     detector = apriltag.Detector(options, searchpath=apriltag._get_dll_path())
 
     for stream in input_streams:
 
-        video = cv2.VideoCapture(1)
+        video = cv2.VideoCapture(0)
 
         output = None
 
@@ -58,106 +75,141 @@ def apriltag_video(input_streams=['../media/input/single_tag.mp4', '../media/inp
             output = cv2.VideoWriter(output_path, codec, fps, (width, height))
 
         
-
-        while(video.isOpened()):
-
-            success, frame = video.read()
-            if not success:
+        while(1): 
+            if EXITPROGRAMFLAG == 1:
                 break
-            robotCoordinates, poseMatrix1, poseMatrix2, overlay = apriltag.detect_tags(frame,
-                                                   detector,
-                                                   camera_params= (1094.285869, 1110.03366, 586.92316, 306.77308),
-                                                   tag_size=0.0762,
-                                                   vizualization=3,
-                                                   verbose=3,
-                                                   annotation=True)
+            # IF ROBOT IS NOT RETURNING TO ORIGIN AND IS AWAITING  COMMAND, THEN HAVE THE USER 
+            # INPUT DESTINATION COORDINATES
+            if (robotReturnToOrigin == ROBOTNOTRETURNING and robotCommandInProgress == WAITINGFORCOMMAND):
+                destCoord = [0 , 0]
+                destCoord[0] = int(input("ENTER X COORDINATE FOR DESTINATION: "))
+                destCoord[1] = int(input("ENTER Y COORDINATE FOR DESTINATION: "))
+                robotCommandInProgress = COMMANDINPROGRESS
 
-            # coordinate array flag is asserted when april tag is detected and robotCoordinate array fills up
-            coordinate_array_flag = numpy.any(robotCoordinates)
+            # ELSE THE ROBOT IS RETURNING TO ORIGIN SO THE DESTINATION COORDINATES HAVE TO BE SET TO ORIGIN
+            # set to 50 so point is visible on screen
+            else:
+                destCoord[0] = 50
+                destCoord[1] = 50
+                robotReturnToOrigin = ROBOTRETURNINGTOORIGIN
 
-            # test destination coordinate
-            destCoord = [1000 , 800]
+            while(robotCommandInProgress == COMMANDINPROGRESS):
 
-            
-
-            # only prints if coordinate array is filled (if apriltag is detected)
-            if (coordinate_array_flag != False):
-                
-                # calculating absolute robot angle
-                if poseMatrix2[0] >=0:
-                    robotAngle =  (numpy.arccos(poseMatrix1[0]) / math.pi) * 180
-                else:
-                    robotAngle =  (numpy.arccos(poseMatrix1[0]) / math.pi) * -180
-                
-                # TRANSPOSED COORDINATES    
-                # we need to transpose the robot coordinates and destination coordinates to be positioned as if
-                # the robot is at the origin. This is to make it easier to calculate the angle between two vectors.
-                destCoordTransposed = [(destCoord[0] - robotCoordinates[0]), (destCoord[1] - robotCoordinates[1])]
-
-                transposedRobotCoordinates = [0, 0]
-                
-                destinationAngleFromXAxis = 180 / math.pi * numpy.arctan(destCoordTransposed[1]/destCoordTransposed[0])
-
-
-                destinationAngle = 0
-
-                # variable name change for easier read
-                destX = destCoordTransposed[0]
-                destY = destCoordTransposed[1]
-
-
-                # CHECK "APRILTAG ANGLE CASES.JPG" to understand math
-
-                # case 1
-                if (destX>0 and (destY)>0):
-                    destinationAngle = destinationAngleFromXAxis
-                
-                # case 2
-                elif (destX<0 and destY>0):
-                    destinationAngleFromXAxis = 180 / math.pi * numpy.arctan(abs(destCoordTransposed[1])/abs(destCoordTransposed[0]))
-                    destinationAngle = 180 - destinationAngleFromXAxis
-                
-                # case 3
-                elif (destX<0 and destY<0):
-                    destinationAngleFromXAxis = 180 / math.pi * numpy.arctan((destCoordTransposed[1])/(destCoordTransposed[0]))
-                    destinationAngle = -1 * (180 - destinationAngleFromXAxis)
-
-                # case 4
-                elif (destX > 0 and destY < 0):
-                    destinationAngleFromXAxis = 180 / math.pi * numpy.arctan((destCoordTransposed[1])/(destCoordTransposed[0]))
-                    destinationAngle = destinationAngleFromXAxis
-
-                # how close the robot angle to pointing directly at destination address
-                differenceAngle = destinationAngle - robotAngle
-
-                if (differenceAngle > -5 and differenceAngle < 5):
-                    robotPositionedCorrectlySTATE = True
-
-                print("\r robot position: ",robotCoordinates)
-                print("\r robot angle: ", robotAngle)
-                print("\r destination angle from x axis: ", destinationAngleFromXAxis)
-                print("\r destination angle: ", differenceAngle)
-
-            if output_stream:
-                output.write(overlay)
-
-            # because the image is flipped the Y destination point needs to be corrected
-            yErrorResolution = 1100
-
-            if display_stream:
-
-                # flipping the image so that our coordinate systems starts at bottom left of camera
-                overlay = cv2.flip(overlay, 0)
-
-                # displaying destination coordinate as a red circle
-                overlay = cv2.circle(overlay, (destCoord[0], yErrorResolution - destCoord[1]), radius=15, color=(0, 0, 255), thickness=-1)
-                cv2.imshow(detection_window_name, overlay)
-                
-                if cv2.waitKey(1) & 0xFF == ord(' '): # Press space bar to terminate
+                success, frame = video.read()
+                if not success:
                     break
+                robotCoordinates, poseMatrix1, poseMatrix2, overlay = apriltag.detect_tags(frame,
+                                                    detector,
+                                                    camera_params= (1094.285869, 1110.03366, 586.92316, 306.77308),
+                                                    tag_size=0.0762,
+                                                    vizualization=3,
+                                                    verbose=3,
+                                                    annotation=True)
 
-def returnRobotState():
-    return robotPositionedCorrectlySTATE
+                # coordinate array flag is asserted when april tag is detected and robotCoordinate array fills up
+                coordinate_array_flag = numpy.any(robotCoordinates)
+
+                # test destination coordinate
+                
+
+                
+
+                # only prints if coordinate array is filled (if apriltag is detected)
+                if (coordinate_array_flag != False):
+                    
+                    # calculating absolute robot angle
+                    if poseMatrix2[0] >=0:
+                        robotAngle =  (numpy.arccos(poseMatrix1[0]) / math.pi) * 180
+                    else:
+                        robotAngle =  (numpy.arccos(poseMatrix1[0]) / math.pi) * -180
+                    
+                    # TRANSPOSED COORDINATES    
+                    # we need to transpose the robot coordinates and destination coordinates to be positioned as if
+                    # the robot is at the origin. This is to make it easier to calculate the angle between two vectors.
+                    destCoordTransposed = [(destCoord[0] - robotCoordinates[0]), (destCoord[1] - robotCoordinates[1])]
+
+                    transposedRobotCoordinates = [0, 0]
+                    
+                    destinationAngleFromXAxis = 180 / math.pi * numpy.arctan(destCoordTransposed[1]/destCoordTransposed[0])
+
+
+                    destinationAngle = 0
+
+                    # variable name change for easier read
+                    destX = destCoordTransposed[0]
+                    destY = destCoordTransposed[1]
+
+
+                    # CHECK "APRILTAG ANGLE CASES.JPG" to understand math
+
+                    # case 1
+                    if (destX>0 and (destY)>0):
+                        destinationAngle = destinationAngleFromXAxis
+                    
+                    # case 2
+                    elif (destX<0 and destY>0):
+                        destinationAngleFromXAxis = 180 / math.pi * numpy.arctan(abs(destCoordTransposed[1])/abs(destCoordTransposed[0]))
+                        destinationAngle = 180 - destinationAngleFromXAxis
+                    
+                    # case 3
+                    elif (destX<0 and destY<0):
+                        destinationAngleFromXAxis = 180 / math.pi * numpy.arctan((destCoordTransposed[1])/(destCoordTransposed[0]))
+                        destinationAngle = -1 * (180 - destinationAngleFromXAxis)
+
+                    # case 4
+                    elif (destX > 0 and destY < 0):
+                        destinationAngleFromXAxis = 180 / math.pi * numpy.arctan((destCoordTransposed[1])/(destCoordTransposed[0]))
+                        destinationAngle = destinationAngleFromXAxis
+
+                    # how close the robot angle to pointing directly at destination address
+                    differenceAngle = destinationAngle - robotAngle
+
+                    if (differenceAngle > -5 and differenceAngle < 5):
+                        robotPositionedCorrectlySTATE = 2
+                    else:
+                        robotPositionedCorrectlySTATE = 1
+
+                    
+
+                    # calculate how close the robot is to the destination
+                    robotDistancefromDestination = destCoord[0] - robotCoordinates[0], destCoord[1] - robotCoordinates[1]
+
+                    if (robotDistancefromDestination[0] > -100 and robotDistancefromDestination[0] < 100):
+                        if (robotDistancefromDestination[1] > -100 and robotDistancefromDestination[1] < 100):
+                            if (robotCommandInProgress == COMMANDINPROGRESS):
+                                robotReturnToOrigin = ROBOTRETURNINGTOORIGIN
+                                break
+                            else:
+                                robotReturnToOrigin = ROBOTNOTRETURNING
+                                robotCommandInProgress = WAITINGFORCOMMAND
+                                break
+
+                    print("\r robot position: ",robotCoordinates)
+                    print("\r robot distance from destination: ", robotDistancefromDestination)
+                    print("\r destination coordinate: ", destCoord)
+                    print("\r robot angle: ", robotAngle)
+                    print("\r destination angle from x axis: ", destinationAngleFromXAxis)
+                    print("\r destination angle: ", differenceAngle)
+
+                if output_stream:
+                    output.write(overlay)
+
+                # because the image is flipped the Y destination point needs to be corrected
+                yErrorResolution = 1100
+
+                if display_stream:
+
+                    # flipping the image so that our coordinate systems starts at bottom left of camera
+                    overlay = cv2.flip(overlay, 0)
+
+                    # displaying destination coordinate as a red circle
+                    overlay = cv2.circle(overlay, (destCoord[0], yErrorResolution - destCoord[1]), radius=15, color=(0, 0, 255), thickness=-1)
+                    cv2.imshow(detection_window_name, overlay)
+                    
+                    if cv2.waitKey(1) & 0xFF == ord(' '): # Press space bar to terminate
+                        EXITPROGRAMFLAG = 1
+                        break
+
 ################################################################################
 
 if __name__ == '__main__':
