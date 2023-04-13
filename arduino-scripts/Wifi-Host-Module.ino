809a297066d27fd8bd2837cff9a598f054a90683
AaronMGibbs <100796840+AaronMGibbs@users.noreply.github.com>
2023-04-12 23:36:49 -0400

    Update Wifi-Host-Module.ino

--- arduino-scripts/Wifi-Host-Module.ino
+++ arduino-scripts/Wifi-Host-Module.ino
@@ -1,4 +1,4 @@
-///Host Wifi Module
+///Host Wifi Module (Sender
 
 #include <ESP8266WiFi.h>
 #include <espnow.h>
@@ -9,14 +9,11 @@
 // REPLACE WITH THE MAC Address of your receiver 
 uint8_t broadcastAddress[] = {0x8C, 0xAA, 0xB5, 0x0C, 0xDF, 0x34};
 
-//these variables determine where the robot will turn and enable it to go
-uint8_t state;
+unsigned long lastTime = 0;  
+unsigned long timerDelay = 2000;  // send readings timer
 
-// Define variables to store incoming readings
-// int for incoming obstacle is used to determine what direction the obstruction is in (1 for left, 2 for center, 3 for right)
-int incomingobstacle;
-
-
+//this variable will determine what state the robot is in 
+int state;
 
 // Variable to store if sending data was successful
 String success;
@@ -24,19 +21,17 @@ String success;
 //Structure example to send data
 //Must match the receiver structure
 typedef struct send_message {
-    uint8_t ste;
+    int ste;
  
 } send_message;
 
-typedef struct recieve_message {
-    int obst;
-} recieve_message;
+
 
 // Create a struct_message to hold directions to send to deliveroid
 send_message apriltag;
 
 // Create a struct_message to hold incoming value from the deliveroid
-recieve_message incomingReading;
+
 
 // Callback when data is sent
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
@@ -49,16 +44,10 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
   }
 }
 
-// Callback when data is received
-void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
-  memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
-  Serial.print("Bytes received: ");
-  Serial.println(len);
-  incomingobstacle=recieve_message.obst;
 }
 //////////////////This is where we'll do apriltag calculations//////////////////////////
 void aprilcalc(){
-  ste=0;
+  state=0;
 }
 
 
@@ -67,12 +56,11 @@ void setup() {
   // Init Serial Monitor
   Serial.begin(115200);
 
-  // Init DHT sensor
-  //dht.begin();
+ 
  
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
+
 
   // Init ESP-NOW
   if (esp_now_init() != 0) {
@@ -81,23 +69,18 @@ void setup() {
   }
 
   // Set ESP-NOW Role
-  esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
 
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
   esp_now_register_send_cb(OnDataSent);
   
-  // Register peer
-  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
   
-  // Register for a callback function that will be called when data is received
-  esp_now_register_recv_cb(OnDataRecv);
+  
 }
  
 void loop() {
-  bool globalenable;
+
   
-  if (globalenable==0) {
+   if ((millis() - lastTime) > timerDelay) {
     
 
     //Get apriltag readings
@@ -109,7 +92,7 @@ void loop() {
     // Send message via ESP-NOW
     esp_now_send(broadcastAddress, (uint8_t *) &apriltag, sizeof(apriltag));
 
- 
+ lastTime = millis();
   }
 }
  
